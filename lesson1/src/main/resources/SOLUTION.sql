dc3d8d8c0e933a84fac659283190f67b7d95b741
Roman Gulevich <r.i.gulevich@gmail.com>
2023-01-27 19:38:57 +0300

    Added sqripts to lesson 1

--- lesson1/src/main/resources/SOLUTION.sql
+++ lesson1/src/main/resources/SOLUTION.sql
@@ -1,2 +1,5 @@
-
-
+create table Student (id bigint AUTO_INCREMENT primary key,name varchar,birthday date,"group" int);
+create table Subject (id bigint AUTO_INCREMENT primary key,name varchar,description varchar,grade int);
+create table PaymentType (id bigint AUTO_INCREMENT primary key,name varchar);
+create table Payment (id bigint AUTO_INCREMENT primary key,type_id bigint references PaymentType(id),amount decimal,student_id bigint references Student(id),payment_date timestamp);
+create table Mark (id bigint AUTO_INCREMENT primary key,student_id bigint references Student(id),subject_id bigint references Subject(id),mark int);
