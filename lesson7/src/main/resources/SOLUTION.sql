4af51bcc8e3be1d428e585d32e192b0d19dc7dd5
Roman Gulevich <r.i.gulevich@gmail.com>
2023-01-28 15:29:49 +0300

    Fixed bugs in lesson 7

--- lesson7/src/main/resources/SOLUTION.sql
+++ lesson7/src/main/resources/SOLUTION.sql
@@ -2,4 +2,13 @@ SELECT * FROM mark WHERE mark > 6 ORDER BY mark DESC;
 SELECT * FROM payment WHERE amount < 300 ORDER BY amount;
 SELECT * FROM paymenttype ORDER BY name;
 SELECT * FROM student ORDER BY name DESC;
-SELECT DISTINCT s.id, s.name, s.birthday, s."group" FROM student AS s JOIN payment AS p ON s.id = p.student_id WHERE p.amount > 1000 ORDER BY s.birthday;
+select distinct s.id, s.name, s.birthday, s.groupnumber from STUDENT AS s join PAYMENT p on s.ID = p.STUDENT_ID where AMOUNT > 1000 order by s.BIRTHDAY;
+
+
+
+
+
+
+
+
+
