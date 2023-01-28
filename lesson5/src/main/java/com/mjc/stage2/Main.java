98538ee5324e458d3e8caaf7658980902290920c
Roman Gulevich <r.i.gulevich@gmail.com>
2023-01-28 12:46:02 +0300

    Fixed bugs in lesson 5

---
+++ lesson5/src/main/java/com/mjc/stage2/Main.java
@@ -0,0 +1,54 @@
+package com.mjc.stage2;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+
+    private static String driver = "org.h2.Driver";
+    private static final String DB_URL = "jdbc:h2:file:C:\\Users\\USER\\Desktop\\db_SQL_task\\University;DB_CLOSE_DELAY=-1;MODE=MySQL;NON_KEYWORDS=GROUP,USER";
+
+    private static String CREATE = "lesson5/src/main/resources/CREATE.sql";
+    private static String PAYMENT = "lesson5/src/main/resources/PAYMENT.sql";
+    private static String PAYMENTTYPE = "lesson5/src/main/resources/PAYMENTTYPE.sql";
+    private static String STUDENT = "lesson5/src/main/resources/STUDENT.sql";
+
+    public static void main(String[] args) throws RuntimeException {
+
+        try {
+            Class.forName(driver);
+            Connection connection = DriverManager.getConnection(DB_URL);
+            Statement statement = connection.createStatement();
+
+            BufferedReader paymenttype = new BufferedReader(new FileReader(new File(PAYMENTTYPE)));
+            insert(paymenttype, statement);
+
+            BufferedReader student = new BufferedReader(new FileReader(new File(STUDENT)));
+            insert(student, statement);
+
+            BufferedReader payment = new BufferedReader(new FileReader(new File(PAYMENT)));
+            insert(payment, statement);
+
+            statement.close();
+            connection.close();
+
+        } catch (SQLException | ClassNotFoundException | IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+
+    private static void insert(BufferedReader br, Statement statement) throws IOException, SQLException {
+        String currentLine = br.readLine();
+        while (!(currentLine == null)) {
+            statement.executeUpdate(currentLine);
+            currentLine = br.readLine();
+        }
+    }
+}
